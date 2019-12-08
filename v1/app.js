56f22ebbe86bb090b9b7765d03ce296ea9efc4f3
root <siningtong@gmail.com>
2019-12-08 05:44:30 +0000

    configured mangodb and mongoose to insert and get data from database

--- v1/app.js
+++ v1/app.js
@@ -1,44 +1,67 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 3000
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true})
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const campgroundaSchema=new mongoose.Schema({
+	name:String,
+	image:String
+})
+const Campground = mongoose.model('Campground',campgroundaSchema)
 
-let campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://www.shopskibluemt.com/content/images/thumbs/0000569_yeti-mountain-campsite_625.jpeg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://koa.com/blog/images/make-campsite-feel-like-home.jpg?preset=blogPhoto'
-  }, {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  },
-  {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  },
-  {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  },
-  {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  },
-  {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  },
-  {
-    name: 'Mountain Goat',
-    image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-  }
-]
+// Campground.create( {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   }, (err,campground)=>{
+// 	if(err){
+// 		console.log(err)
+// 	} else{
+// 		console.log('Newly created campground')
+// 		console.log(campground)
+// 	}
+// })
+
+
+// let campgrounds = [
+//   {
+//     name: 'Salmon Creek',
+//     image: 'https://www.shopskibluemt.com/content/images/thumbs/0000569_yeti-mountain-campsite_625.jpeg'
+//   },
+//   {
+//     name: 'Granite Hill',
+//     image: 'https://koa.com/blog/images/make-campsite-feel-like-home.jpg?preset=blogPhoto'
+//   }, {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   },
+//   {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   },
+//   {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   },
+//   {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   },
+//   {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   },
+//   {
+//     name: 'Mountain Goat',
+//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
+//   }
+// ]
 
 
 
@@ -47,16 +70,24 @@ app.get('/', (req, res) => {
 })
 
 app.get('/campgrounds', (req, res) => {
-
-  res.render('campgrounds', { campgrounds })
+	//get all campgrounds from db
+	Campground.find({},(err,campgrounds)=>{
+		if(err){
+			console.log(err)
+		}	else{
+			  res.render('campgrounds', { campgrounds })
+		}
+		
+	})
 })
 
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name
   const image = req.body.image
   const newCampground = { name, image }
-  campgrounds.push(newCampground)
-  res.redirect('/campgrounds')
+  Campground.create(newCampground,(err,newlyCreatedCampground)=>{
+	  err?console.log(err):res.redirect('/campgrounds')
+  })
 })
 
 app.get('/campgrounds/new', (req, res) => {
@@ -67,5 +98,5 @@ app.get('/campgrounds/new', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log('server started!')
+  // console.log('server started!')
 })
