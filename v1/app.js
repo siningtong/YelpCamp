fa20bcb6129febf22e612cc6254f8bbc01569d0c
root <siningtong@gmail.com>
2019-12-08 19:54:08 +0000

    add show page to show details of certain campground,add description input for submition form

--- v1/app.js
+++ v1/app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const app = express()
 const mongoose = require('mongoose')
 const PORT = 3000
-mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true})
@@ -12,86 +11,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const campgroundaSchema=new mongoose.Schema({
 	name:String,
-	image:String
+	image:String,
+	description:String
 })
 const Campground = mongoose.model('Campground',campgroundaSchema)
 
-// Campground.create( {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   }, (err,campground)=>{
-// 	if(err){
-// 		console.log(err)
-// 	} else{
-// 		console.log('Newly created campground')
-// 		console.log(campground)
-// 	}
+// Campground.create({
+// 	name:'Nanaimo',
+// 	image: 'https://www.shopskibluemt.com/content/images/thumbs/0000569_yeti-mountain-campsite_625.jpeg',
+// 	description:'it is beautiful'
 // })
-
-
-// let campgrounds = [
-//   {
-//     name: 'Salmon Creek',
-//     image: 'https://www.shopskibluemt.com/content/images/thumbs/0000569_yeti-mountain-campsite_625.jpeg'
-//   },
-//   {
-//     name: 'Granite Hill',
-//     image: 'https://koa.com/blog/images/make-campsite-feel-like-home.jpg?preset=blogPhoto'
-//   }, {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   },
-//   {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   },
-//   {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   },
-//   {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   },
-//   {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   },
-//   {
-//     name: 'Mountain Goat',
-//     image: 'https://boyslifeorg.files.wordpress.com/2014/04/tent-featured.jpg?w=700&h=525'
-//   }
-// ]
-
 
 
 app.get('/', (req, res) => {
   res.render('landing')
 })
-
+//index route,show all the campgrounds
 app.get('/campgrounds', (req, res) => {
 	//get all campgrounds from db
 	Campground.find({},(err,campgrounds)=>{
 		if(err){
 			console.log(err)
 		}	else{
-			  res.render('campgrounds', { campgrounds })
+			  res.render('index', { campgrounds })
 		}
 		
 	})
 })
-
+//create route,add new campground
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name
   const image = req.body.image
-  const newCampground = { name, image }
+  const description = req.body.description
+  const newCampground = { name, image, description }
   Campground.create(newCampground,(err,newlyCreatedCampground)=>{
 	  err?console.log(err):res.redirect('/campgrounds')
   })
 })
-
+//New route,show form to submit new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('new')
+})
+//Show route,show more info of campground
+app.get('/campgrounds/:id', (req,res)=>{
+	console.log(req.params.id)
+	const campgroundId = req.params.id
+	Campground.findById(campgroundId,(err,foundCampground)=>{
+	err?console.log(err):res.render('show',{foundCampground} )
+})	
 })
 
 
