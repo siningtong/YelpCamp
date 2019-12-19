19363e62496abcdb45ac783e6bbe9004aacc6696
root <siningtong@gmail.com>
2019-12-19 05:07:25 +0000

    styled comments

--- v1/app.js
+++ v1/app.js
@@ -8,6 +8,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true})
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"))
 const Comment = require("./models/comments")
 const Campground = require("./models/campground")
 const seedDB = require("./seeds")
